f3ff0f0c7f3f7f5ad8820b6bfd8374159568b998
Michelle Child <michelle@crew2030.org>
2022-02-17 11:39:29 -0500

    Splash Page, okay for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,9 @@ const IndexPage = () => {
       <section>
         <div className="container">
           <img src={logo} alt="M"  className="mb-3" />
-          <h1 className="title is-1 has-text-primary"><span className="name-style px-2 has-background-primary-light">Hi! I’m Michelle.</span></h1>
-          <p className="is-size-5 mb-2 is-family-sans-serif has-text-weight-semibold has-text-grey-light">product designer | developer</p>
-          <h2 className="is-size-3">I aim to design tools that make your work a more joyful experience. <span className="is-size-6 px-1 has-text-weight-semibold has-text-info">(like writing with your favorite pen)</span></h2>
-          <h2 className="is-size-3">I'm currently the <span className="has-text-weight-bold">Director of Design</span> at <a className="is-link" href="https://www.crew2030.org" rel="noreferrer" target="_blank">Crew2030</a>. My role is to imagine and design intuitive user experiences across the Crew Platform with a clean, reusable code structure.</h2>
+          <h1 className="title is-1 has-text-primary">Hi! I’m Michelle</h1>
+          <p className="subtitle is-size-3">I aim to design joyful software. <span className="simile is-size-6 px-1">like writing with your favorite pen ✍🏼</span></p>
+          <p className="is-size-3">I currently serve as the <span className="has-text-weight-bold">Director of Design</span> and <span className="has-text-weight-bold">Co Founder</span> of <a className="is-link" href="https://www.crew2030.org" rel="noreferrer" target="_blank">Crew2030</a>, a tech nonprofit and white label community platform for over 75 member organizations, each doing their part for a better world.</p>
           </div>
       </section>
     </Layout>
