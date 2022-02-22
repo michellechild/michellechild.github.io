a33a316a4c11317f2099e8a5535e7e3c4d775fd1
Michelle Child <michelle@crew2030.org>
2022-02-22 09:13:56 -0500

    styling changes to splash page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ const IndexPage = () => {
       <section>
         <div className="container">
           <img src={logo} alt="M"  className="mb-3" />
-          <h1 className="title is-1 has-text-primary">Hi! I’m Michelle</h1>
-          <p className="subtitle is-size-3">I aim to design joyful software. <span className="simile is-size-6 px-1">like writing with your favorite pen ✍🏼</span></p>
-          <p className="is-size-3">I currently serve as the <span className="has-text-weight-bold">Director of Design</span> and <span className="has-text-weight-bold">Co Founder</span> of <a className="is-link" href="https://www.crew2030.org" rel="noreferrer" target="_blank">Crew2030</a>, a tech nonprofit and white label community platform for over 75 member organizations, each doing their part for a better world.</p>
+          <h1 className="title is-1 has-text-primary">Hi! I’m Michelle.</h1>
+          <p className="is-size-3">I aim to design <span className="rainbow-text">joyful</span> software. <span className="simile is-size-6">✍🏼 like writing with your favorite pen </span></p>
+          <p className="is-size-3-desktop is-size-4">I currently serve as the <span className="has-text-weight-bold">Director of Design</span> and <span className="has-text-weight-bold">Co Founder</span> of <a className="is-link" href="https://www.crew2030.org" rel="noreferrer" target="_blank">Crew2030</a>, a tech nonprofit and white label community platform for over 75 member organizations, each doing their part for a better world.</p>
           </div>
       </section>
     </Layout>
